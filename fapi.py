a7aad0ddbc1b16d589cb78e564e1701db7231efd
Ian Myjer <imyjer@protonmail.com>
2019-08-17 15:21:17 -0400

    add file upload feature

--- fapi.py
+++ fapi.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile
 from pydantic import BaseModel
 from sklearn.linear_model import LogisticRegression
 from joblib import load
@@ -6,6 +6,7 @@ import pandas as pd
 import json
 
 def preprocess(df):
+    df = df.dropna(axis=0)
     # process into proper format
     df_ = pd.get_dummies(df)
     return df_.reindex(columns = model_columns, fill_value = 0)
@@ -47,4 +48,16 @@ async def titanic(data: TitanicData):
     # create JSON object and return
     return json.dumps({'prediction': str(prediction)})
 
+@app.post('/titanic_upload/')
+async def titanic_upload(file: UploadFile = File(...)):
+    df = pd.read_csv(file.file)
+    df = df[['Age','Sex','Embarked']]
+
+    query = preprocess(df)
+
+    # make prediction using preprocessed data
+    prediction = list(model.predict(query))
+
+    # create JSON object and return
+    return json.dumps({'prediction': str(prediction)})
 
--- request.py
+++ request.py
@@ -3,11 +3,10 @@ import requests
 import json
 
 url = 'http://127.0.0.1:8000/titanic/'
-params = {"Age": 18, "Sex": 'female', "Embarked": 'S'}
-# params = [{"Age":18,"Sex":"female","Embarked":"S"},{"Age":21,"Sex":"male","Embarked":"S"}]
+params = {"age": 18, "sex": "female", "embarked": "S"}
+# params = [{"age":18,"sex":"female","embarked":"S"},{"age":21,"sex":"male","embarked":"S"}]
 
-# response = requests.get(url, params)
-response = requests.post(url, data = json.dumps(params))
+response = requests.post(url, json = params)
 
 print(response.json())
 
